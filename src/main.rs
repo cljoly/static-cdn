b89e9388a9d82a4af920d9054875c84a2f7b3241
Clément Joly <foss@131719.xyz>
2024-12-27 21:46:19 +0000

    perf: improve hashing performance reading chunks

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 use std::env;
 use std::error::Error;
 use std::fs::File;
+use std::hash::Hasher as _;
 use std::io::Read;
 
 use rayon::prelude::*;
@@ -13,6 +14,7 @@ use twox_hash::XxHash64;
 use walkdir::WalkDir;
 
 const SEED: u64 = 0x431C_71C5_AD99_39B4;
+const CHUNK_SIZE: usize = 1 << 16;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut args = env::args();
@@ -32,9 +34,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     all_files.par_iter().for_each(|entry| {
         let path = entry.path();
         let mut f = File::open(path).unwrap();
-        let mut b = Vec::new();
-        f.read_to_end(&mut b).unwrap();
-        let hash = XxHash64::oneshot(SEED, &b);
+        let mut b = [0u8; CHUNK_SIZE];
+        let mut hasher = XxHash64::with_seed(SEED);
+        loop {
+            let n = f.read(&mut b).unwrap();
+            // This will hash trailing null bytes, but it's fine: if a file differs only by null
+            // bytes, for our purpose, we can deem it equal.
+            hasher.write(&b);
+            if n == 0 {
+                break;
+            }
+        }
+        let hash = hasher.finish();
         println!("{hash} {}", path.display());
     });
 
