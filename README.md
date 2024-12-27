cc1aba36aea5b818d8ee5497f465cea285cdfb48
Clément Joly <foss@131719.xyz>
2024-12-27 20:43:49 +0000

    feat: add quick and dirty hashing

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # static-cdn
 
 Manage entries cached in the CDN of your static site.
+In other words, a CDN cache invalidation tool for your static site.
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,27 @@
 
 use std::env;
 use std::error::Error;
+use std::fs::File;
+use std::io::Read;
 
+use twox_hash::XxHash3_128;
 use walkdir::WalkDir;
+
+const SEED: u64 = 0x431C_71C5_AD99_39B4;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut args = env::args();
     let _ = args.next().unwrap(); // Throw away the binary’s name
     for entry in WalkDir::new(args.next().unwrap()).max_open(dbg!(args.next().unwrap()).parse()?) {
         let entry = entry.unwrap();
-        println!("{}", entry.path().display());
+        if entry.file_type().is_file() {
+            let path = entry.path();
+            let mut f = File::open(path)?;
+            let mut b = Vec::new();
+            f.read_to_end(&mut b)?;
+            let hash = XxHash3_128::oneshot_with_seed(SEED, &b);
+            println!("{hash} {}", path.display());
+        }
     }
 
     Ok(())
