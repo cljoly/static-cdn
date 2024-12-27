6c630066d09e351af653a69ec2b916025c7fddc0
Clément Joly <foss@131719.xyz>
2024-12-27 21:10:34 +0000

    feat: compute hashes in parallel using rayon
    There is lots of room for improvements, but this is a first step.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 
-use twox_hash::XxHash3_128;
+use rayon::prelude::*;
+
+use twox_hash::XxHash64;
 use walkdir::WalkDir;
 
 const SEED: u64 = 0x431C_71C5_AD99_39B4;
@@ -15,17 +17,26 @@ const SEED: u64 = 0x431C_71C5_AD99_39B4;
 fn main() -> Result<(), Box<dyn Error>> {
     let mut args = env::args();
     let _ = args.next().unwrap(); // Throw away the binary’s name
-    for entry in WalkDir::new(args.next().unwrap()).max_open(dbg!(args.next().unwrap()).parse()?) {
-        let entry = entry.unwrap();
-        if entry.file_type().is_file() {
-            let path = entry.path();
-            let mut f = File::open(path)?;
-            let mut b = Vec::new();
-            f.read_to_end(&mut b)?;
-            let hash = XxHash3_128::oneshot_with_seed(SEED, &b);
-            println!("{hash} {}", path.display());
-        }
-    }
+    let all_files = WalkDir::new(args.next().unwrap())
+        .max_open(args.next().unwrap().parse()?)
+        .into_iter()
+        .filter_map(|entry| {
+            let entry = entry.unwrap();
+            if entry.file_type().is_file() {
+                Some(entry)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+    all_files.par_iter().for_each(|entry| {
+        let path = entry.path();
+        let mut f = File::open(path).unwrap();
+        let mut b = Vec::new();
+        f.read_to_end(&mut b).unwrap();
+        let hash = XxHash64::oneshot(SEED, &b);
+        println!("{hash} {}", path.display());
+    });
 
     Ok(())
 }
